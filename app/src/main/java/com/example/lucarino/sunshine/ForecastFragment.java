4c628310f6044882a18ecff3fc3e41c3652e6316
Luis Carino <luiscarino@outlook.com>
2016-03-18 18:21:22 -0400

    Introducing service and removing async task.

--- app/src/main/java/com/example/lucarino/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/lucarino/sunshine/ForecastFragment.java
@@ -21,7 +21,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.lucarino.sunshine.data.WeatherContract;
-import com.example.lucarino.sunshine.util.FetchWeatherTask;
+import com.example.lucarino.sunshine.service.SunshineService;
 
 
 /**
@@ -176,9 +176,10 @@ public class ForecastFragment extends Fragment implements android.support.v4.app
      * Executes an async call to fetch forecast.
      */
     public void updateForecastWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        final String location = Utility.getPreferredLocation(getActivity());
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, location);
+        getActivity().startService(intent);
     }
 
     /**
@@ -241,9 +242,7 @@ public class ForecastFragment extends Fragment implements android.support.v4.app
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        updateForecastWeather();
     }
 
     public interface OnForecastListener {
