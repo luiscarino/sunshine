06cb71d386a19231ba6b227a4eb5a0219f9c1b0a
Luis Carino <luiscarino@outlook.com>
2016-05-13 14:05:02 -0400

    Implementing notifications.

--- app/src/main/java/com/example/lucarino/sunshine/Utility.java
+++ app/src/main/java/com/example/lucarino/sunshine/Utility.java
@@ -168,5 +168,17 @@ public class Utility {
         return -1;
     }
 
+
+    public static String formatTemperature(Context context, double temperature) {
+        // Data stored in Celsius by default.  If user prefers to see in Fahrenheit, convert
+        // the values here.
+        String suffix = "\u00B0";
+        if (!isMetric(context)) {
+            temperature = (temperature * 1.8) + 32;
+        }
+
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        return String.format(context.getString(R.string.format_temperature), temperature);
+    }
 }
 
